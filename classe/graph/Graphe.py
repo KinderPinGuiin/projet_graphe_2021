a7a8dfd8240c6edb998ab769bace3c5615afd80f
EdouarRouch <edouard.rch@gmail.com>
2021-10-20 11:59:22 +0200

    merge de graph

--- classe/graph/Graphe.py
+++ classe/graph/Graphe.py
@@ -11,8 +11,48 @@ class Graph(IGraphe):
         self.__page_dict = dict([("Les colos de Bernard", Page("Les colos de Bernard"))])
         self.__user_dict = dict([("Dupont", Utilisateur("Dupont", "Bernard", 56))])
 
-    def nb_nodes(self):
+    def nb_nodes(self) -> int:
         return len(self.__nodes)
+
+    
+    def nb_lines(self) -> int:
+        result = 0
+        for i in self.get_nodes():
+            result += len(i.get_succ_list())
+        return result
+
+
+    def get_nodes(self) -> list[GraphCellule]:
+        return self.__nodes
+
+
+    def get_page_dict(self) -> dict:
+        return self.__page_dict
+
+
+    def get_user_dict(self) -> dict:
+        return self.__user_dict
+    
+
+    def get_node_by_name(self, name: str) -> Sommet:
+        node_index = self.get_user_dict().get(name) or self.get_page_dict().get(name)
+        if node_index != None:
+            return self.get_nodes()[node_index].get_node()
+        return node_index
+    
+
+    def get_nb_pages(self) -> int:
+        return len(self.get_page_dict())
+
+
+    def get_nb_users(self) -> int:
+        return len(self.get_user_dict())
+
+
+    def is_node_in(self, node: Sommet) -> bool:
+        return node.get_name() in self.get_user_dict() or \
+                node.get_name() in self.get_page_dict()
+
 
     def get_avg_age(self):
         total = 0
@@ -27,7 +67,7 @@ class Graph(IGraphe):
             page_name = page.get_name()
             admins_names[page_name] = list[str]
             for user in page.get_admins():
-                admin_names[page_name].append(user.get_name())
+                admins_names[page_name].append(user.get_name())
 
         return admins_names
 
@@ -36,12 +76,56 @@ class Graph(IGraphe):
         return
     """
 
+
+    # Commandes
+
+    def add_node(self, node: Sommet) -> None:
+        assert node != None
+
+        nodes = self.get_nodes()
+        node_name = node.get_name()
+
+        if not self.is_node_in(node):
+            cell = GraphCellule(node, list[Sommet])
+            nodes.append(cell)
+
+            if isinstance(node, Page):
+                self.get_page_dict()[node_name] = nodes.index(cell)
+            else:
+                self.get_user_dict()[node_name] = nodes.index(cell)
+    
+
+    def delete_node(self, node: Sommet) -> None:
+        assert node != None
+
+        nodes = self.get_nodes()
+        node_name = node.get_name()
+
+        if self.is_node_in(node): 
+            if isinstance(node, Page):
+                index = self.get_page_dict().get(node_name)
+                del self.get_page_dict()[node_name]
+            else:
+                index = self.get_user_dict().get(node_name)
+                del self.get_user_dict()[node_name]
+            del nodes[index]
+            self.__update_dict(index)
+
+    
+    def __update_dict(self, index: int) -> None:
+        # TODO
+        return None
+            
+
+
+
     # Outils
 
     """
     Renvoie la liste des clés du dictionnaire __page_dict fusionnée à la liste
     des clés de __user_dict.
     """
+    
     def __merge_keys(self):
         keys = self.__page_dict.keys()
         keys.append(self.__user_dict.keys())
