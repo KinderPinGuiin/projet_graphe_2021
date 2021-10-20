957cdc5f455cd01bfdd071b1f7f74000d692b5bf
KinderPinGuiin <jordan.teltel.2001@gmail.com>
2021-10-20 14:34:08 +0200

    Ajout méthode get_increasing_nodes

--- classe/graph/Graphe.py
+++ classe/graph/Graphe.py
@@ -14,45 +14,36 @@ class Graph(IGraphe):
     def nb_nodes(self) -> int:
         return len(self.__nodes)
 
-    
     def nb_lines(self) -> int:
         result = 0
         for i in self.get_nodes():
             result += len(i.get_succ_list())
         return result
 
-
     def get_nodes(self) -> list[GraphCellule]:
         return self.__nodes
-
 
     def get_page_dict(self) -> dict:
         return self.__page_dict
 
-
     def get_user_dict(self) -> dict:
         return self.__user_dict
-    
 
     def get_node_by_name(self, name: str) -> Sommet:
         node_index = self.get_user_dict().get(name) or self.get_page_dict().get(name)
-        if node_index != None:
+        if node_index is not None:
             return self.get_nodes()[node_index].get_node()
         return node_index
-    
 
     def get_nb_pages(self) -> int:
         return len(self.get_page_dict())
 
-
     def get_nb_users(self) -> int:
         return len(self.get_user_dict())
-
 
     def is_node_in(self, node: Sommet) -> bool:
         return node.get_name() in self.get_user_dict() or \
                 node.get_name() in self.get_page_dict()
-
 
     def get_avg_age(self):
         total = 0
@@ -71,16 +62,14 @@ class Graph(IGraphe):
 
         return admins_names
 
-    """
     def get_increasing_name_nodes(self):
-        return
-    """
-
+        sorted_keys = sorted(self.__merge_keys(), key=str.lower)
+        return [self.get_node_by_name(x) for x in sorted_keys]
 
     # Commandes
 
     def add_node(self, node: Sommet) -> None:
-        assert node != None
+        assert node is not None
 
         nodes = self.get_nodes()
         node_name = node.get_name()
@@ -93,7 +82,6 @@ class Graph(IGraphe):
                 self.get_page_dict()[node_name] = nodes.index(cell)
             else:
                 self.get_user_dict()[node_name] = nodes.index(cell)
-    
 
     def delete_node(self, node: Sommet) -> None:
         assert node != None
@@ -101,7 +89,7 @@ class Graph(IGraphe):
         nodes = self.get_nodes()
         node_name = node.get_name()
 
-        if self.is_node_in(node): 
+        if self.is_node_in(node):
             if isinstance(node, Page):
                 index = self.get_page_dict().get(node_name)
                 del self.get_page_dict()[node_name]
@@ -111,13 +99,9 @@ class Graph(IGraphe):
             del nodes[index]
             self.__update_dict(index)
 
-    
     def __update_dict(self, index: int) -> None:
         # TODO
         return None
-            
-
-
 
     # Outils
 
@@ -125,9 +109,8 @@ class Graph(IGraphe):
     Renvoie la liste des clés du dictionnaire __page_dict fusionnée à la liste
     des clés de __user_dict.
     """
-    
+
     def __merge_keys(self):
         keys = self.__page_dict.keys()
         keys.append(self.__user_dict.keys())
         return keys
-
